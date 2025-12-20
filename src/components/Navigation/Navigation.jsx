36d6c48dbef39d631fedff1b055c10c3de819c03
MarcPickel <marcpc@att.net>
2025-12-20 15:22:31 -0600

    Add: icons and styles to Header, Banner, and Nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,32 +1,32 @@
 import "./Navigation.css";
 
-import swords from "../../assets/swords-icon.svg";
-import feather from "../../assets/feather-icon.svg";
-import bookmark from "../../assets/bookmark-icon.svg";
-import book from "../../assets/book-icon.svg";
-import home from "../../assets/home-icon.svg";
+import swords from "../../assets/swords-icon-white.svg";
+import feather from "../../assets/feather-icon-white.svg";
+import bookmark from "../../assets/bookmark-icon-white.svg";
+import book from "../../assets/book-icon-white.svg";
+import calendar from "../../assets/calendar-icon-white.svg";
 
 function Navigation() {
   return (
     <nav className="nav">
       <button className="nav__button" type="button">
-        <img className="nav__button-icon" src="" alt="" />
+        <img className="nav__button-icon" src={book} alt="Book" />
         Library
       </button>
       <button className="nav__button" type="button">
-        <img className="nav__button-icon" src="" alt="" />
+        <img className="nav__button-icon" src={bookmark} alt="Bookmark" />
         My Books
       </button>
       <button className="nav__button" type="button">
-        <img className="nav__button-icon" src="" alt="" />
+        <img className="nav__button-icon" src={calendar} alt="Calendar" />
         This Year's
       </button>
       <button className="nav__button" type="button">
-        <img className="nav__button-icon" src="" alt="" />
+        <img className="nav__button-icon" src={swords} alt="Swords" />
         Wisdom & Virtue
       </button>
       <button className="nav__button" type="button">
-        <img className="nav__button-icon" src="" alt="" />
+        <img className="nav__button-icon" src={feather} alt="Feather" />
         About
       </button>
     </nav>
