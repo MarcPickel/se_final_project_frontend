6a1495eefdf1c9c33aa5dfc1d734392deaa0952c
MarcPickel <marcpc@att.net>
2025-12-20 13:33:53 -0600

    Delete: unnecessary fonts; Add: favicon & nav styles

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,22 +1,29 @@
+import "./Navigation.css";
+
 function Navigation() {
   return (
-    <div className="nav">
+    <nav className="nav">
       <button className="nav__button" type="button">
+        <img className="nav__button-icon" src="" alt="" />
         Library
       </button>
       <button className="nav__button" type="button">
+        <img className="nav__button-icon" src="" alt="" />
         My Books
       </button>
       <button className="nav__button" type="button">
+        <img className="nav__button-icon" src="" alt="" />
         This Year's
       </button>
       <button className="nav__button" type="button">
+        <img className="nav__button-icon" src="" alt="" />
         Wisdom & Virtue
       </button>
       <button className="nav__button" type="button">
+        <img className="nav__button-icon" src="" alt="" />
         About
       </button>
-    </div>
+    </nav>
   );
 }
 
